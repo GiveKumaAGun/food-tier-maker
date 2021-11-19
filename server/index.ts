6fb1681e93262e9f2010410ef0c281c969bd288e
GiveKumaAGun <reijitsusaka@gmail.com>
2021-11-20 05:08:58 +0900

    Feat: added image uploading to cloud storage

--- server/index.ts
+++ server/index.ts
@@ -22,38 +22,49 @@ const bucket = storage.bucket("test-bucket-please-ignore");
 
 app.get('/', (req, res) => {
   res.send('Landing')
-})
+});
 
 app.get('/api/hello', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/api/image/', multer.single('file'), (req, res, next) => {
+app.get('/api/lists/:listId/images', (req, res) => {
+  
+});
+
+app.post('/api/image', multer.single('file'), (req, res, next) => {
+  console.log(req.query)
   if (!req.file) {
     res.status(400).send('No file uploaded.');
     return;
   }
 
   // Create a new blob in the bucket and upload the file data.
-  const blob = bucket.file(req.file.originalname);
-  const blobStream = blob.createWriteStream({
-    resumable: false,
-  });
+  if (typeof req.query.imageId === "string") {
+    const blob = bucket.file(req.query.imageId);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+  
+    blobStream.on('error', err => {
+      next(err);
+    });
+  
+  
+    blobStream.on('finish', () => {
+      // The public URL can be used to directly access the file via HTTP.
+      const publicUrl = format(
+        `https://storage.googleapis.com/${bucket.name}/${req.query.imageId}`
+      );
+      res.status(200).send(publicUrl);
+    });
+  
+    blobStream.end(req.file.buffer);
 
-  blobStream.on('error', err => {
-    next(err);
-  });
-
-
-  blobStream.on('finish', () => {
-    // The public URL can be used to directly access the file via HTTP.
-    const publicUrl = format(
-      `https://storage.googleapis.com/${bucket.name}/${"test123"}`
-    );
-    res.status(200).send(publicUrl);
-  });
-
-  blobStream.end(req.file.buffer);
+    if (typeof req.query.prevImageId === "string" && req.query.prevImageId !== "") {
+      bucket.file(req.query.prevImageId).delete()
+    }
+  }
 });
 
 // Start the server
